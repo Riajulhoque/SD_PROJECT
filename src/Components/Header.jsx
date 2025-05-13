a42b986cb5c417c09e08033aacab199ef355258f
safin_A07 <safinhossain@9339@gmail.com>
2025-05-13 22:00:28 +0600

    navbar done and marquee done

--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -1,10 +1,13 @@
 import React from 'react';
 import logo from "../assets/logo.png";
+import { format} from "date-fns";
 const Header = () => {
     return (
         <div className='flex justify-center flex-col items-center'>
-           <img className='w-[350px]' src={logo} alt="" />
-           <p>Journalism Without Fear or Favour</p>
+           <img className='w-[400px]' src={logo} alt="" />
+           <p className='text-accent'>Journalism Without Fear or Favour</p>
+           <p className='font-semibold text-accent'>{format(new Date() ,"EEEE , MMMM MM , yyyy ")}</p>
+           <p className='font-semibold text-accent'>Time : {format(new Date() ," h : m :s")}</p>
         </div>
     );
 };
